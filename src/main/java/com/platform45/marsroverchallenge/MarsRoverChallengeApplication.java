ff3a8b8cfda2c9e5a222b0dd5fd3c06dccfb6669
Thabang Phaleng <thabang@space.tm>
2018-04-23 09:08:46 +0200

    initial project push

---
+++ src/main/java/com/platform45/marsroverchallenge/MarsRoverChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.platform45.marsroverchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverChallengeApplication.class, args);
+	}
+}
